d55608a83e8a0310d26335c522e8ff502d84b2d0
Hanna <marmalademarmoset@gmail.com>
2023-05-25 12:36:31 -0700

    Made functioning crafts state in CategoryDetails component

--- src/CategoryDetails.js
+++ src/CategoryDetails.js
@@ -1,46 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link, useLocation, useNavigate} from "react-router-dom";
+import { useParams, Link, useLocation, useNavigate } from "react-router-dom";
 import CraftCard from "./CraftCard";
 
 const CategoryDetails = ({ categories, onRemoveCategory }) => {
   let { id } = useParams();
-  const navigate = useNavigate()
-   id = parseInt(id)
+  const navigate = useNavigate();
+  id = parseInt(id);
   const location = useLocation();
   const editUrl = location.pathname + "/edit";
   const newCraftUrl = location.pathname + "/new";
   // const category = categories.find((c) => c.id == id);
 
   const [category, setCategory] = useState({
-    name:"",
-    image:'',
-    description:"",
-    crafts: []
+    name: "",
+    image: "",
+    description: "",
+    crafts: [],
+  });
 
-  })
+  const [crafts, setCrafts] = useState([]);
 
-
-useEffect(() => {
-
-  const cat = categories.find((c) => c.id == id);
-  if (cat) {
-    setCategory(cat)
-  }
-}, [categories]);
-
-
+  useEffect(() => {
+    const cat = categories.find((c) => c.id == id);
+    if (cat) {
+      setCategory(cat);
+      const selectedCrafts = category.crafts;
+      if (selectedCrafts) {
+        setCrafts(selectedCrafts);
+      }
+    }
+  }, [categories, category]);
 
   const handleDeleteClick = (deletedCategory) => {
-    console.log("id from in handledeleteclick", deletedCategory.id)
+    console.log("id from in handledeleteclick", deletedCategory.id);
     fetch(`http://localhost:9292/categories/${deletedCategory.id}`, {
       method: "DELETE",
     })
-    .then (() => onRemoveCategory(deletedCategory))
-    .then(navigate('/categories'))
+      .then(() => onRemoveCategory(deletedCategory))
+      .then(navigate("/categories"));
   };
 
-
-  return (category) ? (
+  return category ? (
     <div>
       <h1 className="title">{category.name}</h1>
       <div className="content">
@@ -63,25 +63,27 @@ useEffect(() => {
         <div className="text">
           <h2>{category.name} Projects</h2>
         </div>
-        {category.crafts.map((craft) => 
-          <CraftCard key={craft.id} craft={craft} category={category} />
-        )}
-
+        {/* </div> used to be category.crafts */}
+        {category.crafts !== "{}"
+          ? crafts.map((craft) => (
+              <CraftCard key={craft.id} craft={craft} category={category} />
+            ))
+          : null}
 
         <br />
         <br />
-      <div>
-        <h4>Difficulty:</h4>
-        <p> 💚 Easy, 🧡🧡 Medium, ❤️❤️❤️ Hard </p>
-        <div className="btn" id="projectbtn">
-          <Link to={newCraftUrl}>
-            <button>Add a {category.name} Craft</button>
-          </Link>
+        <div>
+          <h4>Difficulty:</h4>
+          <p> 💚 Easy, 🧡🧡 Medium, ❤️❤️❤️ Hard </p>
+          <div className="btn" id="projectbtn">
+            <Link to={newCraftUrl}>
+              <button>Add a {category.name} Craft</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  ) : null
+  ) : null;
 };
 
 export default CategoryDetails;
